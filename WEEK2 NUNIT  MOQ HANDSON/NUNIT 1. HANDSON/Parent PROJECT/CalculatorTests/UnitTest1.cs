246e82e9a17f8c83618d1fcb9de6f7821ac06ca9
kinshuk3962 <155351790+kinshuk3962@users.noreply.github.com>
2025-06-28 15:52:21 +0530

    Add files via upload

---
+++ WEEK2 NUNIT  MOQ HANDSON/NUNIT 1. HANDSON/Parent PROJECT/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calc = null;
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(10, 5, 15)]
+        [TestCase(-1, -1, -2)]
+        public void Add_WhenCalled_ReturnsSum(int a, int b, int expected)
+        {
+            var result = calc.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
