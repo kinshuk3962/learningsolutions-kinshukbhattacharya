7845e714777cd1934c152b4a44bb8de7c4674db1
kinshuk3962 <155351790+kinshuk3962@users.noreply.github.com>
2025-07-12 21:08:56 +0530

    Add files via upload

---
+++ WEEK 4 WEBAPI HANDSON CODE/HANDSON 6 CODE/KafkaConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Confluent.Kafka;
+
+class KafkaConsumer
+{
+    static void Main()
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+        consumer.Subscribe("chat-topic");
+
+        Console.WriteLine("Listening for chat messages...");
+
+        while (true)
+        {
+            var msg = consumer.Consume();
+            Console.WriteLine($"Received: {msg.Message.Value}");
+        }
+    }
+}
