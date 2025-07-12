e94aad4a8648e545492dae2de6cd622e509d6114
kinshuk3962 <155351790+kinshuk3962@users.noreply.github.com>
2025-07-12 21:10:23 +0530

    Add files via upload

---
+++ WEEK 4 WEBAPI HANDSON CODE/HANDSON 6 CODE/KafkaPublisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+
+class KafkaPublisher
+{
+    static async Task Main()
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        Console.WriteLine("Type messages to send (type 'exit' to quit):");
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input?.ToLower() == "exit") break;
+
+            await producer.ProduceAsync("chat-topic", new Message<Null, string> { Value = input });
+            Console.WriteLine("Message sent!");
+        }
+    }
+}
