be691b1a503327820d25a074f362b4511e34d6cc
kinshuk3962 <155351790+kinshuk3962@users.noreply.github.com>
2025-07-27 22:11:08 +0530

    Add files via upload

---
+++ WEEK 6 React handson code/myfirstreact/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Assuming you still want to use App.css for basic styling
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the first session of React</h1> {/*  */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
